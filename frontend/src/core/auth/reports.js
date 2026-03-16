08c3c8d7dfa85b3765edbc64c1a8278e6b18cad8
CamiloGuengue <90392520+CamiloGuengue@users.noreply.github.com>
2026-03-16 02:35:52 -0500

    Update reports.js

--- frontend/src/core/auth/reports.js
+++ frontend/src/core/auth/reports.js
@@ -4,7 +4,7 @@ const PYTHON_API_URL = 'https://kairo-integrative-project-turing-production-b3f6
 // REPORTE INDIVIDUAL - CODER
 // ════════════════════════════════════════
 async function generateReportCoder(coderId)
-    event.stopPropagation();{
+    event.stopPropagation();
     const btn = event.target;
     const originalText = btn.textContent;
 
@@ -45,7 +45,7 @@ async function generateReportCoder(coderId)
 // REPORTE GRUPAL - CLAN
 // ════════════════════════════════════════
 async function generateReportClan(clan)
-    event.stopPropagation();{
+    event.stopPropagation();
     const btn = event.target;
     const originalText = btn.textContent;
 
